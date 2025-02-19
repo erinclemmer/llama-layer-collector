c4b29fe549ed6d09697a58807d62fe5aa0e83842
erinclemmer <erin.c.clemmer@gmail.com>
2025-02-19 15:28:59 -0600

    clean up

--- test.py
+++ test.py
@@ -11,18 +11,14 @@ MODEL_DIR_1B: str = 'models/Llama3.2-1b-instruct'
 DATA_DIR_8B: str = 'data/Meta-Llama-3-8B'
 MODEL_DIR_8B: str = 'models/Meta-Llama-3-8B'
 
-LAYERS_1B = 146
-LAYERS_8B = 291
+NUM_KEYS_1B = 146
+NUM_KEYS_8B = 291
 
 class LayerDataCollecterTests(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
-        """
-        Start up the initial bootstrap node(s) needed for the tests.
-        """
-        pass
-        cls.assertTrue(os.path.exists(MODEL_DIR_1B), "Model does not exist for testing, please download Llama3.2-1b-instruct")
+        cls.assertTrue(os.path.exists(MODEL_DIR_1B), "1B Model does not exist for testing, please download Llama3.2-1b-instruct")
+        cls.assertTrue(os.path.exists(MODEL_DIR_8B), "8B Model does not exist for testing, please download Llama3.2-1b-instruct")
 
     def setUp(self):
         if os.path.exists(DATA_DIR_1B):
@@ -32,26 +28,25 @@ class LayerDataCollecterTests(unittest.TestCase):
 
     def test_cache_1B(self):
         collector = LayerDataCollector(MODEL_DIR_1B, DATA_DIR_1B)
-        self.assertEqual(len(collector.layer_files.keys()), LAYERS_1B)
+        self.assertEqual(len(collector.layer_files.keys()), NUM_KEYS_1B)
         self.assertEqual(len(collector.layer_size_cache), collector.num_layers)
         self.assertTrue(os.path.exists(DATA_DIR_1B))
         self.assertTrue(os.path.exists(collector._cache_file()))
         with open(collector._cache_file(), 'r') as f:
             cache = json.load(f)
-            self.assertEqual(len(cache['layer_files'].keys()), LAYERS_1B)
+            self.assertEqual(len(cache['layer_files'].keys()), NUM_KEYS_1B)
             self.assertEqual(len(cache['layer_sizes']), collector.num_layers)
 
     def test_cache_8B(self):
         collector = LayerDataCollector(MODEL_DIR_8B, DATA_DIR_8B)
-        self.assertEqual(len(collector.layer_files.keys()), LAYERS_8B)
+        self.assertEqual(len(collector.layer_files.keys()), NUM_KEYS_8B)
         self.assertEqual(len(collector.layer_size_cache), collector.num_layers)
         self.assertTrue(os.path.exists(DATA_DIR_8B))
         self.assertTrue(os.path.exists(collector._cache_file()))
         with open(collector._cache_file(), 'r') as f:
             cache = json.load(f)
-            self.assertEqual(len(cache['layer_files'].keys()), LAYERS_8B)
+            self.assertEqual(len(cache['layer_files'].keys()), NUM_KEYS_8B)
             self.assertEqual(len(cache['layer_sizes']), collector.num_layers)
-
 
 # If you want to run these tests directly from the command line:
 if __name__ == '__main__':
